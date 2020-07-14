8d5e5632091b30acc2bc7c9134c4ecccb82cb096
yzhang <yzhang@chaindigg.com>
2020-07-14 21:43:11 +0800

    初始化项目

---
+++ src/main/java/com/example/javademo/controller/ZYController.java
@@ -0,0 +1,14 @@
+package com.example.javademo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ZYController {
+
+    @RequestMapping(value = "/demo")
+    public String demo() {
+        return "success";
+    }
+}
